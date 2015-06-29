29148b6bb38ab8b25cb2fae3cd5c0aa5b4f80228
eligo <596840317@qq.com>
2015-06-29 14:26:52 +0800

    timer nearest fixed

--- common/timer/timer.c
+++ common/timer/timer.c
@@ -146,7 +146,7 @@ void timer_tick(struct timer_t * timer) {
 uint32_t timer_nearest(struct timer_t * timer) {
 	uint32_t current = timer->current;
 	uint32_t n = 0;
-	for (; current < timer->listn; ++curren, ++n) {
+	for (; current < timer->listn; ++current, ++n) {
 		if (timer->list[current].head)
 			break;
 	}
